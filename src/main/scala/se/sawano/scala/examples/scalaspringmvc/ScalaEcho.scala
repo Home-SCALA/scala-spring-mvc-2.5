70cb95b56e884b391f8e85fe0a6107de936c89f2
Sasha <skmets@gmail.com>
2017-01-04 15:18:01 +0200

    master

--- src/main/scala/se/sawano/scala/examples/scalaspringmvc/ScalaEcho.scala
+++ src/main/scala/se/sawano/scala/examples/scalaspringmvc/ScalaEcho.scala
@@ -2,7 +2,4 @@ package se.sawano.scala.examples.scalaspringmvc
 
 import reflect.BeanProperty
 
-/**
- * If we are using the Jackson Scala module we can remove the @BeanProperty annotations.
- */
 case class ScalaEcho(@BeanProperty val name: String, @BeanProperty val weekDay: String)
